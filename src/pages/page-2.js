3f1c93add90afe480c1eb7c1168e4dc954526f7c
Obed <obed.imp@gmail.com>
2019-10-21 21:22:06 +0200

    Add styled-components and funny messages to friends

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,9 +7,9 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <h1>Vinieron a que les siguiera diciendo maricos?</h1>
+    <p>AYYYYYYYYYY</p>
+    <Link to="/">Go back to mariquisimos</Link>
   </Layout>
 )
 
